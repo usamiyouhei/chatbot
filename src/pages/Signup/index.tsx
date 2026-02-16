84d5ddd924ceac76a200e8661b8340b60fd15bac
youhei usami <usao9110@icloud.com>
2026-02-16 09:13:22 +0400

    2/16

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,30 @@
-import { Link } from 'react-router-dom';
-import './index.css';
+import { Link } from "react-router-dom";
+import "./index.css";
+import { useState } from "react";
+import { authRepository } from "../../modules/auth/auth.repository";
 
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signup = async () => {
+    setIsLoading(true);
+    try {
+      const { user, token } = await authRepository.signup(
+        name,
+        email,
+        password,
+      );
+      console.log(user, token);
+    } catch (error) {
+      console.error(error);
+      alert("ユーザー登録に失敗しました");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="auth-container">
       <div className="auth-card">
@@ -10,7 +33,14 @@ export default function Signup() {
         <div className="auth-form-container">
           <div className="form-group">
             <label htmlFor="username">ユーザー名</label>
-            <input id="username" type="text" placeholder="山田太郎" required />
+            <input
+              id="username"
+              type="text"
+              placeholder="山田太郎"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
@@ -20,6 +50,8 @@ export default function Signup() {
               type="email"
               placeholder="example@example.com"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -31,10 +63,17 @@ export default function Signup() {
               placeholder="8文字以上"
               required
               minLength={8}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <button type="button" className="auth-button" onClick={() => {}}>
+          <button
+            type="button"
+            className="auth-button"
+            disabled={isLoading || !name || !email || !password}
+            onClick={signup}
+          >
             アカウント作成
           </button>
         </div>
