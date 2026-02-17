89028b10952d163dba192f1e23e39c1a4fd5db9f
youhei usami <usao9110@icloud.com>
2026-02-17 11:47:48 +0400

    2/17

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,8 +1,13 @@
-import Sidebar from '../../components/Sidebar';
-import './index.css';
-import { Outlet } from 'react-router-dom';
+import { useAtomValue } from "jotai";
+import Sidebar from "../../components/Sidebar";
+import "./index.css";
+import { Navigate, Outlet } from "react-router-dom";
+import { currentUserAtom } from "../../modules/auth/current-user.state";
 
 export default function Chat() {
+  const currentUser = useAtomValue(currentUserAtom);
+
+  if (!currentUser) return <Navigate to="/signin" />;
   return (
     <div className="chat-layout">
       <Sidebar />
