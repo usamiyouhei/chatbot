3bec838c818e91b5208bfdc2be94e4911249e99b
youhei usami <usao9110@icloud.com>
2026-02-27 12:01:13 +0400

    2/27

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,7 +1,11 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI, type Content } from "@google/generative-ai";
 
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY as string;
 const genAi = new GoogleGenerativeAI(API_KEY);
-export const model = genAi.getGenerativeModel({
+const model = genAi.getGenerativeModel({
   model: "gemini-2.5-flash-lite",
 });
+
+export const startChatSesson = (history: Content[]) => {
+  return model.startChat({ history });
+};
